9cfb8868ea4aa67584ed1f0ce06623197ed8cabb
Peter Morrell <pmorrell@x-134-84-43-182.cfans.umn.edu>
2019-08-13 13:57:11 -0500

    add recursive search of fast5

--- Scripts/Guppy_basecalling.sh
+++ Scripts/Guppy_basecalling.sh
@@ -11,4 +11,4 @@ GUPPY=/Applications/ont-guppy-cpu/bin/guppy_basecaller
 OUTPUT=/Users/pmorrell/Downloads/Guppy_Output/"$SAMPLE_NAME"/basecalled
 
 mkdir -p $OUTPUT
-$GUPPY --input_path $FAST5 --save_path $OUTPUT --flowcell $FLOWCELL --kit $KIT
+$GUPPY --recursive --input_path $FAST5 --save_path $OUTPUT --flowcell $FLOWCELL --kit $KIT
