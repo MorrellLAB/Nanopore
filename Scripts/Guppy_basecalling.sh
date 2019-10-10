09dac5ac4da7137592e24b79893c85712c4877a0
Peter Morrell <pmorrell@umn.edu>
2019-10-10 17:15:22 -0500

    adjusting qsub settings

--- Scripts/Guppy_basecalling.sh
+++ Scripts/Guppy_basecalling.sh
@@ -2,7 +2,7 @@
 #   Written by Peter L. Morrell
 #   07 August 2019, Falcon Heights, MN
 
-#PBS -l mem=2000mb,nodes=1:ppn=6,walltime=96:00:00 
+#PBS -l nodes=1:ppn=6,walltime=96:00:00 
 #PBS -m abe 
 #PBS -M pmorrell@umn.edu 
 #PBS -q mesabi
@@ -10,7 +10,7 @@
 set -euf - o pipefail
 
 #   The configuation file contains the input file path sample name
-CONFIG=/panfs/roc/scratch/pmorrell/WBDC355/WBDC355_Run8_basecalling_Config
+CONFIG=/panfs/roc/scratch/pmorrell/WBDC355/WBDC355_Run5/WBDC355_Run5_basecalling_Config
 source $CONFIG
 
 OUTPUT="$OUT_DIR"/"$SAMPLE_NAME"/basecalled_"$GUPPY_VERSION"
