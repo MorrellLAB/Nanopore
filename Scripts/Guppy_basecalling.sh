54686a739e70f2e39882430409cdb5eb57c96116
Peter Morrell <pmorrell@umn.edu>
2019-10-17 09:46:30 -0500

    adding gpu settings

--- Scripts/Guppy_basecalling.sh
+++ Scripts/Guppy_basecalling.sh
@@ -2,15 +2,17 @@
 #   Written by Peter L. Morrell
 #   07 August 2019, Falcon Heights, MN
 
-#PBS -l nodes=1:ppn=6,walltime=96:00:00 
+#PBS -l nodes=1:ppn=24:gpus=2,walltime=24:00:00 
 #PBS -m abe 
 #PBS -M pmorrell@umn.edu 
-#PBS -q mesabi
+#PBS -q k40
+
+module load cuda cuda-sdk
 
 set -euf - o pipefail
 
 #   The configuation file contains the input file path sample name
-CONFIG=/panfs/roc/scratch/pmorrell/WBDC355/WBDC355_Run5/WBDC355_Run5_basecalling_Config
+CONFIG=/panfs/roc/scratch/pmorrell/WBDC355/WBDC355_Run8/WBDC355_Run8_basecalling_Config
 source $CONFIG
 
 OUTPUT="$OUT_DIR"/"$SAMPLE_NAME"/basecalled_"$GUPPY_VERSION"
