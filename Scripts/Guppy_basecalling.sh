e81db89fa2cf9ce6351da46494920630bc5f8a19
Peter Morrell <pmorrell@umn.edu>
2019-11-04 10:29:43 -0600

    adding CPU qsub configuation

--- Scripts/Guppy_basecalling.sh
+++ Scripts/Guppy_basecalling.sh
@@ -2,17 +2,19 @@
 #   Written by Peter L. Morrell
 #   07 August 2019, Falcon Heights, MN
 
-#PBS -l nodes=1:ppn=24:gpus=2,walltime=24:00:00 
+#PBS -l nodes=1:ppn=24,walltime=94:00:00 
 #PBS -m abe 
 #PBS -M pmorrell@umn.edu 
-#PBS -q k40
+#PBS -q mesabi
+#PBS -N guppy_basecalling
 
-module load cuda cuda-sdk
+
+#module load cuda cuda-sdk
 
 set -euf - o pipefail
 
 #   The configuation file contains the input file path sample name
-CONFIG=/panfs/roc/scratch/pmorrell/WBDC355/WBDC355_Run8/WBDC355_Run8_basecalling_Config
+CONFIG=/panfs/roc/scratch/pmorrell/WBDC355_Run8_test/Config
 source $CONFIG
 
 OUTPUT="$OUT_DIR"/"$SAMPLE_NAME"/basecalled_"$GUPPY_VERSION"
