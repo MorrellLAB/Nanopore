2f2057019956a9c2dcd822ea723b7ac8dadc4a2c
Peter Morrell <pmorrell@umn.edu>
2020-02-13 14:43:21 -0600

    updates for GPU basecalling

--- Scripts/Guppy_basecalling.sh
+++ Scripts/Guppy_basecalling.sh
@@ -4,17 +4,18 @@
 
 #PBS -l nodes=1:ppn=24:gpu=2,walltime=12:00:00 
 #PBS -m abe 
-#PBS -M pmorrell@umn.edu 
+#PBS -M gfrascar@umn.edu 
 #PBS -q v100
 #PBS -N guppy_basecalling
 
 # load modules for use of GPU
 module load cuda cuda-sdk
+module load guppy/3.4.4
 
 set -euf - o pipefail
 
 #   The configuation file contains the input file path sample name
-CONFIG=/scratch.global/pmorrell/Morex/Morex_Run12/Config
+CONFIG=/scratch.global/gfrascar/WBDC355/Config_20200109_WBDC355_Run10
 source $CONFIG
 
 #   Output should be written back to data directory
@@ -22,5 +23,7 @@ OUTPUT="$OUT_DIR"/"$SAMPLE_NAME"/basecalled_"$GUPPY_VERSION"
 
 #   Guppy will base call from FAST5 and output compressed fastq, using GPUs on the v100 nodes
 #   Must qsub from a Mesabi node
+#   The '-num_callers' specifies number of callers per GPU, '--device' specifies first 2 GPUs on node
+#   Also using '--compress_fastq' for gzipped fastq output, so '*.fastq.gz'
 mkdir -p $OUTPUT
 $GUPPY --recursive --input_path $FAST5 --compress_fastq --verbose_logs --num_callers 12 --save_path $OUTPUT --device "cuda:0,1" --flowcell $FLOWCELL --kit $KIT
